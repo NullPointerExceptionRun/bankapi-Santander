1d216dbaa13a184557b7e69a0c156d00c46a6325
POE TEU USUARIO <herculessantos306@gmail.com>
2022-05-04 00:11:17 -0300

    chore: add info to movement

--- src/main/java/com/santander/bankapi/model/Movimentacao.java
+++ src/main/java/com/santander/bankapi/model/Movimentacao.java
@@ -1,5 +1,44 @@
 package com.santander.bankapi.model;
 
-public class Movimentacao {
+import java.time.LocalDateTime;
 
+public class Movimentacao {
+	
+	private Integer id;
+	private LocalDateTime datehora;
+	private String descricao;
+	private Double valor;
+	private MovimentacaoTipo tipo;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public LocalDateTime getDatehora() {
+		return datehora;
+	}
+	public void setDatehora(LocalDateTime datehora) {
+		this.datehora = datehora;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Double getValor() {
+		return valor;
+	}
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+	public MovimentacaoTipo getTipo() {
+		return tipo;
+	}
+	public void setTipo(MovimentacaoTipo tipo) {
+		this.tipo = tipo;
+	}
 }
