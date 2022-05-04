42153cffeb58a633ca893c315f61177f804d8dd5
POE TEU USUARIO <herculessantos306@gmail.com>
2022-05-04 15:46:05 -0300

    feat: add map to class account and correntist

--- src/main/java/com/santander/bankapi/model/Conta.java
+++ src/main/java/com/santander/bankapi/model/Conta.java
@@ -1,8 +1,15 @@
 package com.santander.bankapi.model;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
 public class Conta {
 	
+	@Column(name = "conta_numero")
 	private Long numero;
+	
+	@Column(name = "conta_saldo")
 	private Double saldo;
 	
 	public Long getNumero() {
@@ -17,6 +24,4 @@ public class Conta {
 	public void setSaldo(Double saldo) {
 		this.saldo = saldo;
 	}
-	
-
 }
--- src/main/java/com/santander/bankapi/model/Correntista.java
+++ src/main/java/com/santander/bankapi/model/Correntista.java
@@ -1,13 +1,28 @@
 package com.santander.bankapi.model;
 
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name= "tab_correntista")
 public class Correntista {
 
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@Column(length = 20)
 	private String cpf;
+	
+	@Column(length = 60)
 	private String nome;
 	
-	
+	@Embedded
 	private Conta conta;
 	public Conta getConta() {
 		return conta;
@@ -35,7 +50,4 @@ public class Correntista {
 		this.nome = nome;
 	}
 
-	
-	
-	
 }
