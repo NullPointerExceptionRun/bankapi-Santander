b8b59c18038098451c6e7330e5feb1827f7de2a6
POE TEU USUARIO <herculessantos306@gmail.com>
2022-05-04 16:34:24 -0300

    feat: mapped data and made relationship in controllers

--- src/main/java/com/santander/bankapi/controller/CorrentistaController.java
+++ src/main/java/com/santander/bankapi/controller/CorrentistaController.java
@@ -1,5 +1,24 @@
 package com.santander.bankapi.controller;
 
-public class CorrentistaController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.santander.bankapi.model.Correntista;
+import com.santander.bankapi.repository.CorrentistaRepository;
+
+@RestController
+@RequestMapping("/correntistas")
+public class CorrentistaController {
+	
+	@Autowired
+	private CorrentistaRepository repository;
+	
+	@GetMapping
+	public List<Correntista> findAll(){
+		return repository.findAll();
+	}
 }
