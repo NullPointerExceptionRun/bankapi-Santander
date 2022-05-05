c9135ca7353259f11e024d76f2da19e7d5d63e88
POE TEU USUARIO <herculessantos306@gmail.com>
2022-05-04 21:59:59 -0300

    chore: implements to service account

--- src/main/java/com/santander/bankapi/service/MovimentacaoService.java
+++ src/main/java/com/santander/bankapi/service/MovimentacaoService.java
@@ -6,14 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.santander.bankapi.dto.NovaMovimentacao;
+import com.santander.bankapi.model.Correntista;
 import com.santander.bankapi.model.Movimentacao;
 import com.santander.bankapi.model.MovimentacaoTipo;
+import com.santander.bankapi.repository.CorrentistaRepository;
 import com.santander.bankapi.repository.MovimentacaoRepository;
 
 @Service
 public class MovimentacaoService {
 	
 	@Autowired
+	private CorrentistaRepository correntistaRepository;
 	private MovimentacaoRepository repository;
 	public void save(NovaMovimentacao novaMovimentacao) {
 		Movimentacao movimentacao = new Movimentacao();
@@ -25,6 +28,12 @@ public class MovimentacaoService {
 		movimentacao.setTipo(novaMovimentacao.getTipo());
 		movimentacao.setValor(novaMovimentacao.getValor());
 		movimentacao.setValor(valor);
+		
+		Correntista correntista = correntistaRepository.findById(novaMovimentacao.getIdConta()).orElse(null); // se o ID for encontrado pelo id 200, caso contrario NUll
+		if(correntista != null) {
+			correntista.getConta().setSaldo(correntista.getConta().getSaldo() + valor);
+			correntistaRepository.save(correntista);
+		}
 		
 		
 		repository.save(movimentacao);
