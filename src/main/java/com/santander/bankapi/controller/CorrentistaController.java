8a843d2a81c7efe0e11f00efb062a03d117c559f
POE TEU USUARIO <herculessantos306@gmail.com>
2022-05-04 21:36:47 -0300

    feat: add swagger ui

--- src/main/java/com/santander/bankapi/controller/CorrentistaController.java
+++ src/main/java/com/santander/bankapi/controller/CorrentistaController.java
@@ -9,8 +9,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.santander.bankapi.dto.NovoCorrentista;
 import com.santander.bankapi.model.Correntista;
 import com.santander.bankapi.repository.CorrentistaRepository;
+import com.santander.bankapi.service.CorrentistaService;
 
 @RestController
 @RequestMapping("/correntistas")
@@ -19,13 +21,17 @@ public class CorrentistaController {
 	@Autowired
 	private CorrentistaRepository repository;
 	
+	@Autowired
+	private CorrentistaService service;
+	
+	
 	@GetMapping
 	public List<Correntista> findAll(){
 		return repository.findAll();
 	}
 	
 	@PostMapping
-	public void save(@RequestBody Correntista correntista){
-		
+	public void save(@RequestBody NovoCorrentista correntista){
+		service.save(correntista);
 	}
 }
